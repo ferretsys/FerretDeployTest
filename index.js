7fb5769ada2b415856f18a6ef09009d85b1cf49c
cakeGit <cakeistastydev@gmail.com>
2025-03-17 14:17:39 +0000

    Init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fs = require("fs");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello' + fs.readFileSync("served.txt"));
+});
+
+const PORT = 80;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
